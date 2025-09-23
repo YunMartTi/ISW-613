062d4cd2751e989c64ed39c7166779ad82afc2eb
Yunior Martinez <martinezjunior.9095@gmail.com>
2025-09-22 22:14:16 -0600

    Workshop1, mejora en componentes.

--- Workshop_1/index.php
+++ Workshop_1/index.php
@@ -7,8 +7,8 @@
   <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body class="bg-gray-900 flex items-center justify-center h-screen">
-  <div class="bg-gray-700 p-6 rounded-xl shadow-lg text-white text-center">
-    <h1 class="text-3xl font-bold p-2">Reloj Costa Rica</h1>
+  <div class="bg-gray-700 p-8 rounded-xl shadow-lg text-white text-center">
+    <h1 class="text-3xl font-bold p-2">Reloj Costa Rica - PHP</h1>
     <?php
         date_default_timezone_set("America/Costa_Rica");
 
@@ -18,6 +18,11 @@
         echo "<h2>Fecha actual: $fecha</h2>";
         echo "<h2>Hora actual: $hora</h2>";
     ?>
+    <a href="index.html" 
+      class="bg-gray-700 text-white font-semibold px-4 rounded-lg border-2 border-gray-600 hover:bg-gray-600 hover:border-gray-500 transition cursor-pointer">
+      Ver con HTML
+    </a>  
   </div>
+  
 </body>
 </html>
